2f69d3b3010c8a6fe723ee8faa36dd61c16a2b0c
Berker Acır <berkeracir159@gmail.com>
2022-12-11 10:12:17 +0300

    use parser instead model

--- todo_lists.py
+++ todo_lists.py
@@ -2,32 +2,33 @@ from flask_login import login_required, current_user
 from flask_restx import Resource, fields, Namespace
 
 from main import db
-from models import User, List, Item
+from models import User, List
 
 
 api = Namespace(name="todo_lists", description="Management of To-Do Lists")
 
-todo_list_api_model = api.model('To-Do List', {
+api_model = api.model('To-Do List', {
 	'id': fields.Integer(readonly=True, description="id"),
 	'name': fields.String(required=True, description="Name"),
 	'created_at': fields.DateTime(readonly=True, description="Creation Date")
 })
 
-todo_list_put_api_model = api.model('To-Do List (PUT)', {
-	'id': fields.Integer(required=True, description="id"),
-	'name': fields.String(required=True, description="Name"),
-})	# TODO: Should be parameters used? 
+post_parser = api.parser()
+post_parser.add_argument('name', required=True, type=str, help="Name of To-Do List")
 
-todo_list_delete_api_model = api.model('To-Do List (DELETE)', {
-	'id': fields.Integer(required=True, description="id")
-})	# TODO: Should be parameters used? 
+put_parser = api.parser()
+put_parser.add_argument('id', required=True, type=int, help="ID of To-Do List")
+put_parser.add_argument('name', required=True, type=str, help="Name of the To-Do List")
+
+delete_parser = api.parser()
+delete_parser.add_argument('id', required=True, type=int, help="ID of To-Do List")
 
 
 @api.route('/')
 class ToDoLists(Resource):
 	'''Shows all To-Do Lists of authorized user, and gives the user ability of creating, updating, deleting new To-Do Lists'''
 
-	@api.marshal_list_with(todo_list_api_model, code=200)
+	@api.marshal_list_with(api_model, code=200)
 	@api.response(200, "Got To-Do Lists Successfully")
 	@api.response(401, "Unauthorized")
 	@login_required
@@ -37,22 +38,24 @@ class ToDoLists(Resource):
 		todo_lists = List.query.filter_by(user_id=current_user.id).all()
 		return list(map(lambda x: x.serialize(), todo_lists)), 200
 
-	@api.expect(todo_list_api_model)
-	@api.marshal_with(todo_list_api_model, code=201)
+	@api.expect(post_parser)
+	@api.marshal_with(api_model, code=201)
 	@api.response(201, "To-Do List Created Successfully")
 	@api.response(401, "Unauthorized")
 	@login_required
 	def post(self):
 		'''Creates a new To-Do List with given name'''
 
-		new_todo_list = List(user_id=current_user.id, name=api.payload['name'])
+		args = post_parser.parse_args()
+		name = args['name']
+		new_todo_list = List(user_id=current_user.id, name=name)
 		db.session.add(new_todo_list)
 		db.session.commit()
 
 		return new_todo_list.serialize(), 201
 
-	@api.expect(todo_list_put_api_model)
-	@api.marshal_with(todo_list_api_model, code=200)
+	@api.expect(put_parser)
+	@api.marshal_with(api_model, code=200)
 	@api.response(200, "To-Do List Updated Successfully")
 	@api.response(401, "Unauthorized")
 	@api.response(404, "Wrong To-Do List ID")
@@ -60,8 +63,9 @@ class ToDoLists(Resource):
 	def put(self):
 		'''Updates the name of To-Do List with given ID'''
 
-		id = api.payload['id']
-		new_name = api.payload['name']
+		args = put_parser.parse_args()
+		id = args['id']
+		new_name = args['name']
 
 		todo_list = List.query.filter_by(id=id, user_id=current_user.id).first()	# if this returns a To-Do List, then given id exists in database
 
@@ -78,7 +82,7 @@ class ToDoLists(Resource):
 		return todo_list.serialize(), 200
 
 
-	@api.expect(todo_list_delete_api_model)
+	@api.expect(delete_parser)
 	@api.response(200, "To-Do List Deleted Successfully")
 	@api.response(401, "Unauthorized")
 	@api.response(404, "Wrong To-Do List ID")
@@ -86,7 +90,8 @@ class ToDoLists(Resource):
 	def delete(self):
 		'''Deletes the To-Do List with given ID'''
 
-		id = api.payload['id']
+		args = delete_parser.parse_args()
+		id = args['id']
 
 		todo_list = List.query.filter_by(id=id, user_id=current_user.id).first()	# if this returns a To-Do List, then given id exists in database
 
